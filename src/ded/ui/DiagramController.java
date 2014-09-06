8b7d58847837a77a635fe7b6de86a93d92e108a1
Scott McPeak <smcpeak@coverity.com>
2014-09-06 12:12:08 -0700

    move writeImageToPNGFile into new ImageFileUtil class

--- src/ded/ui/DiagramController.java
+++ src/ded/ui/DiagramController.java
@@ -30,23 +30,17 @@ import java.awt.event.MouseMotionListener;
 import java.awt.font.LineMetrics;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
-import java.util.Iterator;
 import java.util.Set;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageWriter;
-import javax.imageio.stream.ImageOutputStream;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -56,6 +50,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import org.json.JSONException;
 
 import util.IdentityHashSet;
+import util.ImageFileUtil;
 import util.Util;
 import util.awt.GeomUtil;
 import util.swing.SwingUtil;
@@ -821,62 +816,10 @@ public class DiagramController extends JPanel
             g.dispose();
 
             // Now, write that image to a file in PNG format.
-            writeImageToPNGFile(bi, file);
+            ImageFileUtil.writeImageToPNGFile(bi, file);
         }
         finally {
             this.setCursor(Cursor.getDefaultCursor());
-        }
-    }
-
-    /** Write the 'bi' to 'file' in PNG format. */
-    public static void writeImageToPNGFile(BufferedImage bi, File file)
-        throws Exception
-    {
-        // This method's code is approximately equivalent to:
-        //   ImageIO.write(bi, "png", file);
-        // except that its file error handling is not atrocious,
-        // it works correctly when 'file' is a symlink,
-        // and I can extend it in the future to write PNG comments.
-
-        // Get a writer than can write PNG.
-        ImageWriter writer;
-        {
-            Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("png");
-            if (!writers.hasNext()) {
-                throw new RuntimeException(
-                    "Unable to write PNG image because there is no registered "+
-                    "image writer for that format.  That might mean the Java "+
-                    "run time library is incomplete somehow.");
-            }
-            writer = writers.next();
-        }
-
-        try {
-            // Open the output file.
-            OutputStream os = new BufferedOutputStream(new FileOutputStream(file));
-            try {
-                ImageOutputStream ios = ImageIO.createImageOutputStream(os);
-                if (ios == null) {
-                    throw new RuntimeException("ImageIO.createImageOutputStream(OutputStream) failed!");
-                }
-
-                try {
-                    // Write the image data to that file.
-                    writer.setOutput(ios);
-                    writer.write(bi);
-                }
-                finally {
-                    ios.close();
-                }
-            }
-            finally {
-                // You have to dig into the source code to discover this, but
-                // closing 'ios' will *not* close 'os'.
-                os.close();
-            }
-        }
-        finally {
-            writer.dispose();
         }
     }
 
